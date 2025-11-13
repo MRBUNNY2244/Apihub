8a3f189c7530b243ba66d23e1d195aaeeb2fbb4e
MRBUNNY2244 <mmrbunny03@gmail.com>
2025-11-13 09:34:17 +0500

    Update otp.js

--- api/otp.js
+++ api/otp.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=tkbjfr2qfv5g9irkefv1l89thj",
+    Cookie: "PHPSESSID=jj6kh5rpi1v4v4iraf4rfdvdf4",
   };
 
   let url;
